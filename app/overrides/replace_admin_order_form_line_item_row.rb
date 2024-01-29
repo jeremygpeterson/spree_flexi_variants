5353dc79ea7f7f120c4b759bbe8ccbf4c5d8e3d3
Collins Lagat <cklagat@gmail.com>
2024-01-29 18:54:30 +0300

    fix: migrate from deface to view overrides

--- app/overrides/replace_admin_order_form_line_item_row.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-  virtual_path: "spree/admin/orders/_line_items",
-  name: "add_additional_line_item_fields_partial_to_admin_order_view",
-  insert_bottom: ".line-item-name",
-  text: "<%= render partial: 'spree/shared/additional_line_item_fields', locals: {item: item} %>"
-)
