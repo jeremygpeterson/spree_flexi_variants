18a0c7608034adc7837bef662d8d2b5e9bb42bd1
Collins Lagat <cklagat@gmail.com>
2024-01-29 12:15:27 +0300

    fix: spree uses a Struct to construct configuration thus can't use old way of extending calculator config

--- lib/spree_flexi_variants/engine.rb
+++ lib/spree_flexi_variants/engine.rb
@@ -3,6 +3,8 @@ module SpreeFlexiVariants
     engine_name 'spree_flexi_variants'
 
     config.autoload_paths += %W(#{config.root}/lib)
+    SpreeCalculators = Struct.new(:shipping_methods, :tax_rates, :promotion_actions_create_adjustments,
+                                  :promotion_actions_create_item_adjustments, :product_customization_types)
 
     # use rspec for tests
     config.generators do |g|
@@ -16,7 +18,23 @@ module SpreeFlexiVariants
 
       Spree::Core::Environment::Calculators.class_eval do
         attr_accessor :product_customization_types
+    config.after_initialize do |app|
+      unless app.config.spree.calculators.respond_to?(:product_customization_types)
+        app.config.spree.calculators = SpreeCalculators.new(
+          shipping_methods: app.config.spree.calculators.shipping_methods,
+          tax_rates: app.config.spree.calculators.tax_rates,
+          promotion_actions_create_adjustments: app.config.spree.calculators.promotion_actions_create_adjustments,
+          promotion_actions_create_item_adjustments: app.config.spree.calculators.promotion_actions_create_item_adjustments,
+          product_customization_types: []
+        )
       end
+      app.config.spree.calculators.product_customization_types += [
+        Spree::Calculator::Engraving,
+        Spree::Calculator::AmountTimesConstant,
+        Spree::Calculator::ProductArea,
+        Spree::Calculator::CustomizationImage,
+        Spree::Calculator::NoCharge
+      ]
     end
 
     config.to_prepare &method(:activate).to_proc
@@ -29,15 +47,5 @@ module SpreeFlexiVariants
         app.config.assets.precompile += ['spree/frontend/spree_flexi_variants_exclusions.js','spree/backend/orders/flexi_configuration.js'] # ,'spree/frontend/spree-flexi-variants.*' # removed for now until we need the styles
     end
 
-    initializer "spree.flexi_variants.register.calculators" do |app|
-      app.config.spree.calculators.add_class('product_customization_types') unless app.config.spree.calculators.respond_to?(:product_customization_types)
-      app.config.spree.calculators.product_customization_types += [
-                                                                    Spree::Calculator::Engraving,
-                                                                    Spree::Calculator::AmountTimesConstant,
-                                                                    Spree::Calculator::ProductArea,
-                                                                    Spree::Calculator::CustomizationImage,
-                                                                    Spree::Calculator::NoCharge
-                                                                   ]
-    end
   end
 end
