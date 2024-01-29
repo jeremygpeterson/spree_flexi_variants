37292b351f918d608144478c31ec8d8e91d50d55
Collins Lagat <cklagat@gmail.com>
2024-01-29 11:52:50 +0300

    feat: clean up install generaator

--- lib/generators/spree_flexi_variants/install/install_generator.rb
+++ lib/generators/spree_flexi_variants/install/install_generator.rb
@@ -1,28 +1,24 @@
 module SpreeFlexiVariants
   module Generators
     class InstallGenerator < Rails::Generators::Base
+      class_option :migrate, type: :boolean, default: false, banner: 'Run SpreeFlexiVariants migrations'
 
       def add_javascripts
-        append_file "vendor/assets/javascripts/spree/frontend/all.js", "//= require spree/frontend/spree_flexi_variants\n"
-        append_file "vendor/assets/javascripts/spree/backend/all.js", "//= require spree/backend/spree_flexi_variants\n"
+        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_flexi_variants\n"
+        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/spree_flexi_variants\n"
       end
-
-      # def add_stylesheets
-        # inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_flexi_variants\n", before: /\*\//, verbose: true
-        # inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_flexi_variants\n", before: /\*\//, verbose: true
-      # end
 
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_flexi_variants'
       end
 
       def run_migrations
-         run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask 'Would you like to run the migrations now? [Y/n]')
-         if run_migrations
-           run 'bundle exec rake db:migrate'
-         else
-           puts 'Skipping rake db:migrate, don\'t forget to run it!'
-         end
+        run_migrations = options[:migrate] || ['', 'y', 'Y'].include?(ask('Would you like to run the migrations now? [Y/n]'))
+        if run_migrations
+          run 'bundle exec rails db:migrate'
+        else
+          puts 'Skipping rails db:migrate, don\'t forget to run it!'
+        end
       end
     end
   end
