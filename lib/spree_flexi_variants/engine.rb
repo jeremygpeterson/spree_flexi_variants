e0385faf4f9131d502ca22ce3b08f2aeb10d6f24
Collins Lagat <cklagat@gmail.com>
2024-01-29 12:20:22 +0300

    format: move method position

--- lib/spree_flexi_variants/engine.rb
+++ lib/spree_flexi_variants/engine.rb
@@ -34,7 +34,6 @@ module SpreeFlexiVariants
       ]
     end
 
-    config.to_prepare &method(:activate).to_proc
 
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
@@ -46,5 +45,6 @@ module SpreeFlexiVariants
         app.config.assets.precompile += ['spree/frontend/spree_flexi_variants_exclusions.js','spree/backend/orders/flexi_configuration.js'] # ,'spree/frontend/spree-flexi-variants.*' # removed for now until we need the styles
     end
 
+    config.to_prepare(&method(:activate).to_proc)
   end
 end
