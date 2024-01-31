6a6b53b1a1642046415453276138652879322f0d
Collins Lagat <cklagat@gmail.com>
2024-01-31 16:35:43 +0300

    feat: move functionality

---
+++ app/services/spree_flexi_variants/product_customizations/create.rb
@@ -0,0 +1,20 @@
+module SpreeFlexiVariants
+  module ProductCustomizations
+    class Create
+      prepend Spree::ServiceModule::Base
+
+      def call(line_item:, product_customization_type:, customized_product_options_attributes:)
+        product_customization = Spree::ProductCustomization.new(
+          line_item: line_item,
+          product_customization_type: product_customization_type,
+          customized_product_options_attributes: customized_product_options_attributes
+        )
+        ApplicationRecord.transaction do
+          return failure(product_customization) unless product_customization.save
+        end
+
+        success(product_customization)
+      end
+    end
+  end
+end
