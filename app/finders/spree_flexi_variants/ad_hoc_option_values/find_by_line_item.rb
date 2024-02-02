7e9c580d64a26ccca4b9aea5fc0ad177a897580b
Collins Lagat <cklagat@gmail.com>
2024-02-02 11:13:32 +0300

    fix: api call throws error when ad_hoc_option_value_ids isn't set in the options field in the payload

--- app/finders/spree_flexi_variants/ad_hoc_option_values/find_by_line_item.rb
+++ app/finders/spree_flexi_variants/ad_hoc_option_values/find_by_line_item.rb
@@ -2,7 +2,7 @@ module SpreeFlexiVariants
   module AdHocOptionValues
     class FindByLineItem
       def execute(line_item:, options:)
-        ad_hoc_option_value_ids = options[:ad_hoc_option_value_ids]
+        ad_hoc_option_value_ids = options[:ad_hoc_option_value_ids] || []
         Spree::AdHocOptionValue.joins(:products)
                                .where(products: { id: line_item.variant.product_id })
                                .where(id: ad_hoc_option_value_ids.reject(&:blank?))
