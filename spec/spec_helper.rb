56e044a69d4e6a2f5169d5d807143ce9bca024d4
Collins Lagat <cklagat@gmail.com>
2024-02-19 22:47:54 +0300

    build(wip): update `spec_helper.rb`

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,10 @@
-require 'simplecov'
-SimpleCov.start 'rails'
-
 # Configure Rails Environment
 ENV['RAILS_ENV'] = 'test'
 
-require File.expand_path('../dummy/config/environment.rb',  __FILE__)
+require File.expand_path('dummy/config/environment.rb', __dir__)
 
-require 'rspec/rails'
-require 'database_cleaner'
-require 'factory_girl'
-require 'ffaker'
-require 'shoulda-matchers'
-require 'pry'
+require 'spree_dev_tools/rspec/spec_helper'
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
@@ -19,22 +12,7 @@ Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
 require 'spree_flexi_variants/factories'
 
 RSpec.configure do |config|
-
-  # Capybara javascript drivers require transactional fixtures set to false, and we use DatabaseCleaner
-  # to cleanup after each test instead.  Without transactional fixtures set to false the records created
-  # to setup a test will be unavailable to the browser, which runs under a seperate server instance.
-  config.use_transactional_fixtures = false
-
-  # If true, the base class of anonymous controllers will be inferred
-  # automatically. This will be the default behavior in future versions of
-  # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
+end
 
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
-  config.order = "random"
-  config.color = true
-  config.infer_spec_type_from_file_location!
 end
